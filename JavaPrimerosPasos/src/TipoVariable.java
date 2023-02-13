5fce9b941fdaea76934abbba12934e3e1559a4d5
Brarias <braianrodrigo14@gmail.com>
2023-02-13 07:59:39 -0300

    Curso de Java por Alura

---
+++ JavaPrimerosPasos/src/TipoVariable.java
@@ -0,0 +1,10 @@
+
+public class TipoVariable {
+	public static void main(String[] args) {
+		double variableUno = 230.89;
+		int variableUnoEntero = (int) variableUno;// cast
+		System.out.println(variableUnoEntero);
+		
+		
+	}
+}
