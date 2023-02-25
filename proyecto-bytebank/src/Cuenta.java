83fba31c89bcdc5f0e400a2803a24b61856b26bb
Brarias <braianrodrigo14@gmail.com>
2023-02-25 03:44:46 -0300

    Encapsulamiento y miembros estáticos

--- proyecto-bytebank/src/Cuenta.java
+++ proyecto-bytebank/src/Cuenta.java
@@ -5,6 +5,21 @@ public class Cuenta {
 	private int numero;
 	private Cliente titular = new Cliente();
 	
+	private static int contadorCuentas = 0;
+
+	public Cuenta() {
+		
+	}
+	
+	public Cuenta(int agencia) {	
+		if(agencia <= 0) {
+			System.out.println("No se permite 0");
+			this.agencia = 1;
+		}
+		this.agencia = agencia;
+		contadorCuentas++;
+		System.out.println("Se van creando: " + contadorCuentas + " cuentas");
+	}
 
 	public void depositar(double saldo) {
 		this.saldo += saldo;
@@ -29,34 +44,37 @@ public class Cuenta {
 		}
 		return retorno;
 	}
-	
+
 	public double getSaldo() {
 		return saldo;
 	}
-	
+
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-	
+
 	public int getAgencia() {
 		return agencia;
 	}
-	
+
 	public void setAgencia(int agencia) {
-		if(agencia > 0) {
-		this.agencia = agencia;
-		}else {
+		if (agencia > 0) {
+			this.agencia = agencia;
+		} else {
 			System.out.println("No esta permitido numeros negativos");
 		}
 	}
-	
+
 	public void setTitular(Cliente titular) {
 		this.titular = titular;
 	}
-	
+
 	public Cliente getTitular() {
 		return titular;
 	}
 	
+	public static int getContadorCuentas() {
+		return contadorCuentas;
+	}
 
 }
---
+++ proyecto-bytebank/src/PruebaConstructor.java
@@ -0,0 +1,14 @@
+
+public class PruebaConstructor {
+	public static void main(String[] args) {
+		
+		Cuenta cuenta1 = new Cuenta(10);
+		Cuenta cuenta2 = new Cuenta(3);
+		Cuenta cuenta3 = new Cuenta(56);
+		System.out.println(Cuenta.getContadorCuentas());
+		
+		
+		
+
+	}
+}
