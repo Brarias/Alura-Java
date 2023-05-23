2d59daf5e3996c02ff2872fc8e80f40286839ce7
Brarias <braianrodrigo14@gmail.com>
2023-05-23 03:46:15 -0300

    se limita el numero de conexiones y se realiza prueba de funcionamiento

--- Java-MySQL/control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
+++ Java-MySQL/control-de-stock/src/main/java/com/alura/jdbc/factory/ConnectionFactory.java
@@ -6,21 +6,22 @@ import javax.sql.DataSource;
 import com.mchange.v2.c3p0.ComboPooledDataSource;
 
 public class ConnectionFactory {
-	
+
 	private DataSource dataSource;
-	
+
 	public ConnectionFactory() {
 		var pooledDataSource = new ComboPooledDataSource();
 		pooledDataSource.setJdbcUrl("jdbc:mysql://localhost/control_de_stock?useTimeZone=true&serverTimeZone=UTC");
 		pooledDataSource.setUser("root");
 		pooledDataSource.setPassword("1234");
-		
+		pooledDataSource.setMaxPoolSize(10);
+
 		this.dataSource = pooledDataSource;
 	}
 
 	public Connection recuperaConexion() throws SQLException {
-		  
+
 		return this.dataSource.getConnection();
 	}
-	
+
 }
